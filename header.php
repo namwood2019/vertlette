2070da10deeb946b6f5e9f3a9f10b79cc5828625
maddoushe <maddoushe@gmail.com>
2020-09-25 01:38:46 -0400

    menu 9

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
         <a href="<?php echo site_url('/cart'); ?>"><i class="fa fa-shopping-cart"></i></a>
       </div>
       
-      <button class="btn-danger "><a href="<?php echo site_url('/wp-login.php'); ?>">Login</a></button>
+      <button class="btn-danger"><a href="<?php echo site_url('/wp-login.php'); ?>">Login</a></button>
       
       <button class="btn-dark"><a href="<?php echo site_url('/wp-login.php?action=register'); ?>">Register</a></button>
     </nav>
