4460d7baed0261fec196d574fc47d0d1023257f1
namwood2019 <58995375+namwood2019@users.noreply.github.com>
2020-09-25 23:10:47 -0400

    Update presentation-content.php

--- template-parts/presentation-content.php
+++ template-parts/presentation-content.php
@@ -125,5 +125,19 @@ Template Name: Presentaion Page
       </div>
     </div>
   </section>
+   
+  <section>
+    <div class="bottom-line"></div>
+          <h3 class="lead">La renaissance</h3>
+          <br/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et molestie ex. 
+          Suspendisse et molestie mauris. Nam ultricies ornare aliquet. Nam euismod rhoncus 
+          tortor, ac efficitur ipsum porttitor quis. Etiam magna turpis, gravida eu facilisis 
+          sed, bibendum eget quam. Integer eu pellentesque purus. Nulla eu est varius, 
+          fermentum risus eget, tristique nulla. Donec iaculis arcu metus, 
+          laoreet ullamcorper quam aliquam maximus. Integer luctus mauris id augue 
+          eleifend, sed efficitur nisl pellentesque. Duis bibendum varius felis.</p>
+      </div>
+  </section>
 
 <?php get_footer(); ?>
